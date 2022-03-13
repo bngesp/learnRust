7d9ce677467c0b759daffda550709d5926f59fba
bassirou ngom <bassiroungom@esp.sn>
2022-03-13 20:55:58 +0000

    itteration

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,33 @@
-enum WebEvent {
-    // An `enum` may either be `unit-like`,
-    PageLoad,
-    PageUnload,
-    // like tuple structs,
-    KeyPress(char),
-    Paste(String),
-    // or c-like structures.
-    Click { x: i64, y: i64 },
-}
-
-// A function which takes a `WebEvent` enum as an argument and
-// returns nothing.
-fn inspect(event: WebEvent) {
-    match event {
-        WebEvent::PageLoad => println!("page loaded"),
-        WebEvent::PageUnload => println!("page unloaded"),
-        WebEvent::PageUnlod(e) => println!("page unloaded {}", e),
-        // Destructure `c` from inside the `enum`.
-        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
-        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
-        // Destructure `Click` into `x` and `y`.
-        WebEvent::Click { x, y } => {
-            println!("clicked at x={}, y={}.", x, y);
-        },
-    }
-}
-
-
-enum VeryVerboseEnumOfThingsToDoWithNumbers {
-    Add,
-    Subtract,
-}
-
-impl VeryVerboseEnumOfThingsToDoWithNumbers {
-    fn run(&self, x: i32, y: i32) -> i32 {
-        match self {
-            Self::Add => x + y,
-            Self::Subtract => x - y,
-        }
-    }
-    fn save(&self, x: i32, y: i32) -> i32 {
-        match self {
-            Self::Add => self.run(x, y) * 2,
-            Self::Subtract => self.run(x, y) * 3,
-        }
-    }
-}
 
 
 fn main (){
+/*    for i in 1..100 {
+        if i % 2 == 0 {  println!("je {value} suis un nombre paire", value=i)}
+        if i % 15 == 0 {
+            println!("fizzbuzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
 
-    let pressed = WebEvent::KeyPress('x');
-    // `to_owned()` creates an owned `String` from a string slice.
-    let pasted  = WebEvent::Paste("my text".to_owned());
-    let click   = WebEvent::Click { x: 20, y: 80 };
-    let load    = WebEvent::PageLoad;
-    let unload  = WebEvent::PageUnload;
+    for a in 1..10 {
+        println!("a={}", a);
+    }
+*/
 
-    inspect(pressed);
-    inspect(pasted);
-    inspect(click);
-    inspect(load);
-    inspect(unload);
-    inspect(unload1);
+    let mut names = vec!["Bob", "Frank", "Ferris"];
 
-    type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+    for name in names.iter_mut() {
+        match name {
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
+        }
+    }
 
-    let x = Operations::run(&Operations::Add, 23, 34);
-    let y = Operations::run(&Operations::Subtract, 23, 34);
-
-    println!("valur {}", x);
-    println!("valeur {}", y);
+    println!("names: {:?}", names);
 }
