e6255e1b7255d9a96a8c399df9c7800566aef9e4
bassirou ngom <bassiroungom@esp.sn>
2022-03-18 21:53:13 +0000

    closure

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,22 @@ fn main() {
     // Imperative approach
     // Declare accumulator variable
     let mut acc = 0;
+    // Iterate: 0, 1, 2, ... to infinity
+    for n in 0.. {
+        // Square the number
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // Break loop if exceeded the upper limit
+            break;
+        } else if is_odd(n_squared) {
+            // Accumulate value, if it's odd
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+
+    // Functional approach
     let sum_of_squared_odd_numbers: u32 =
         (0..).map(|n| n * n)                             // All natural numbers squared
             .take_while(|&n_squared| n_squared < upper) // Below upper limit
