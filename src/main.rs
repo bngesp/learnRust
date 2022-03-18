388e457e56c958b56af3c9ae5a39f37624279174
bassirou ngom <bassiroungom@esp.sn>
2022-03-18 21:53:05 +0000

    closure

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,13 @@ fn main() {
     println!("Find the sum of all the squared odd numbers under 1000");
     let upper = 1000;
 
+    // Imperative approach
+    // Declare accumulator variable
+    let mut acc = 0;
     let sum_of_squared_odd_numbers: u32 =
-        (0..).
+        (0..).map(|n| n * n)                             // All natural numbers squared
+            .take_while(|&n_squared| n_squared < upper) // Below upper limit
+            .filter(|&n_squared| is_odd(n_squared))     // That are odd
+            .fold(0, |acc, n_squared| acc + n_squared); // Sum them
     println!("functional style: {}", sum_of_squared_odd_numbers);
 }
